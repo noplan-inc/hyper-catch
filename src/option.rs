88e53fab55b1b7a3a06e9bf1f4d66566205b9181
serinuntius <wasab1gj@gmail.com>
2022-09-27 08:42:33 +0100

    add config

---
+++ src/option.rs
@@ -0,0 +1,35 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct CliOptions {
+    ///
+    #[clap(short, long, value_parser, default_value_os_t = PathBuf::from("./catch.toml"))]
+    pub config: PathBuf,
+
+    #[clap(short, long, value_parser, default_value_t = false)]
+    pub verbose: bool,
+}
+
+pub enum OutputFormat {
+    Json,
+    Csv,
+}
+
+pub struct NetworkConfig {
+    // chain name
+    pub name: String,
+    // rpc endpoint
+    pub rpc: String,
+    // from block height
+    pub from: u64,
+    // to block height
+    pub to: u64,
+
+    // output path ex) ethereum.json, ethereum.csv
+    pub output: String,
+    // output format
+    pub format: OutputFormat,
+}
