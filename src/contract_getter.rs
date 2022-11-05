7369693a6c5d200bcefc4a3cee324097399b7f55
mo29cg <35455415+mo29cg@users.noreply.github.com>
2022-11-05 16:39:21 +0900

    lifetime error

--- src/contract_getter.rs
+++ src/contract_getter.rs
@@ -1,14 +1,20 @@
-use ethers::{abi::ethereum_types, prelude::*};
+use ethers_contract::Contract;
+use std::{fs, io};
+
+use ethers::{abi::Abi, prelude::*};
 use ethers_providers::{Http, Middleware, Provider};
+
+const ERC721_INTERFACE_ID: [u8; 4] = [128, 172, 88, 205];
+const ERC1155_INTERFACE_ID: [u8; 4] = [217, 182, 122, 38];
 
 pub struct NftContractGetter {
     pub provider: Provider<Http>,
 }
 
 impl NftContractGetter {
-    pub fn new(rpcEndPoint: &str) -> NftContractGetter {
+    pub fn new(rpc_end_point: &str) -> NftContractGetter {
         NftContractGetter {
-            provider: Provider::<Http>::try_from(rpcEndPoint).unwrap(),
+            provider: Provider::<Http>::try_from(rpc_end_point).unwrap(),
         }
     }
 
@@ -44,5 +50,79 @@ impl NftContractGetter {
             contract_addresses.push(receipt.contract_address.unwrap());
         }
         contract_addresses
+    }
+    pub async fn _is_erc721(&self, contract_address: H160) -> Result<bool, io::Error> {
+        let erc721_abi: Abi = serde_json::from_str(&(fs::read_to_string("src/abis/erc721.json")?))?;
+        let contract = Contract::new(contract_address, erc721_abi, &self.provider);
+
+        let support_interface_result = contract
+            .method::<_, bool>("supportsInterface", ERC721_INTERFACE_ID)
+            .unwrap()
+            .call()
+            .await;
+
+        match support_interface_result {
+            Ok(is_erc721) => {
+                return Ok(is_erc721);
+            }
+            _ => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Unsupported,
+                    "supportInterface doesn't exist",
+                ));
+            }
+        }
+    }
+    pub async fn is_erc1155(&self, contract_address: H160) -> Result<bool, io::Error> {
+        let erc1155_abi: Abi =
+            serde_json::from_str(&(fs::read_to_string("src/abis/erc1155.json")?))?;
+        let contract = Contract::new(contract_address, erc1155_abi, &self.provider);
+
+        let support_interface_result = contract
+            .method::<_, bool>("supportsInterface", ERC1155_INTERFACE_ID)
+            .unwrap()
+            .call()
+            .await;
+
+        match support_interface_result {
+            Ok(is_erc1155) => {
+                return Ok(is_erc1155);
+            }
+            _ => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::Unsupported,
+                    "supportInterface doesn't exist",
+                ));
+            }
+        }
+    }
+    pub async fn is_erc721(
+        &self,
+        contract_address: H160,
+    ) -> Result<bool, Box<dyn std::error::Error>> {
+        let erc721_abi: Abi = serde_json::from_str(&(fs::read_to_string("src/abis/erc721.json")?))?;
+        let contract = Contract::new(contract_address, erc721_abi, &self.provider);
+
+        let support_interface_result = contract
+            .method::<_, bool>("supportsInterface", ERC721_INTERFACE_ID)?
+            .call()
+            .await?;
+
+        Ok(support_interface_result)
+    }
+    pub async fn extract_erc721_or_1155(&self, contracts_addresses: Vec<H160>) -> Vec<(H160, u8)> {
+        let mut nft_contracts: Vec<(H160, u8)> = Vec::new();
+
+        for contract_address in contracts_addresses {
+            if self.is_erc721(contract_address).await.unwrap_or(false) {
+                nft_contracts.push((contract_address, 0));
+                continue;
+            }
+
+            if self.is_erc1155(contract_address).await.unwrap_or(false) {
+                nft_contracts.push((contract_address, 1));
+            }
+        }
+        nft_contracts
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use hex;
-
 mod contract;
 mod contract_getter;
 mod formatter;
 mod option;
-
-use ethers_contract::Contract;
 
 use dotenv::dotenv;
 use ethers_core::{abi::Abi, types::Address};
@@ -16,62 +12,14 @@ async fn main() -> anyhow::Result<()> {
     dotenv().ok();
     let rpc = env::var("EthMainRpc").expect("EthMainRpc must be set");
     let getter = contract_getter::NftContractGetter::new(&rpc);
-    // this is a fake address used just for this example
-    let address = "0xC2C747E0F7004F9E8817Db2ca4997657a7746928".parse::<Address>()?;
+    let hashmask = "0xC2C747E0F7004F9E8817Db2ca4997657a7746928".parse::<Address>()?;
+    let uniswap = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D".parse::<Address>()?;
 
-    // (ugly way to write the ABI inline, you can otherwise read it from a file)
-    let abi: Abi = serde_json::from_str(
-        r#"[{
-            "inputs": [
-              {
-                "internalType": "bytes4",
-                "name": "interfaceId",
-                "type": "bytes4"
-              }
-            ],
-            "name": "supportsInterface",
-            "outputs": [
-              {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-              }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-          },
-          {
-            "constant": true,
-            "inputs": [],
-            "name": "name",
-            "outputs": [
-              {
-                "name": "",
-                "type": "string"
-              }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-          }]"#,
-    )?;
+    let contract_addresses = vec![hashmask, uniswap];
 
-    // connect to the network
+    let result = getter.extract_erc721_or_1155(contract_addresses).await;
 
-    // create the contract object at the address
-    let contract = Contract::new(address, abi, getter.provider);
-
-    // let init_value: String = contract.method::<_, String>("name", ())?.call().await?;
-    let init_value = contract
-        .method::<_, bool>("supportsInterface", (hex::decode("80ac58cd").unwrap(),))?
-        .call()
-        .await?;
-    println!("{:?}", init_value);
+    println!("{:?}", result);
 
     Ok(())
 }
-
-// let hashes = getter.get_contract_creation_tx_hashes(3000000).await;
-
-//     let contracts = getter.get_contracts_from_creations(hashes).await;
-//     println!("{:?}", contracts);
